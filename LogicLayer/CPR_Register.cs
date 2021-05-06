021098761355dcb3baf234a5574a4d0493ec4cfb
GustavElbroend <78858884+GustavElbroend@users.noreply.github.com>
2021-05-06 11:18:08 +0200

    Add project files.

---
+++ LogicLayer/CPR_Register.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer;
+
+namespace LogicLayer
+{
+    public class CPR_Register
+    {
+        private Person p;
+        private List<Person> borger;
+        public CPR_Register()
+        {
+            p = new Person();
+            borger = new List<Person>();
+        }
+        public bool validate(uint CPRNumber)
+        {
+            return true;
+            //skal validere om en patient eksistere i databasen
+        }
+
+    }
+}
---
+++ PresentationLayer/ParamedicinUI_VisBatteristatus.cs
@@ -0,0 +1,65 @@
+﻿using RaspberryPiCore.ADC;
+using RaspberryPiCore.JoySticks;
+using RaspberryPiCore.LCD;
+using RaspberryPiCore.TWIST;
+using System;
+using System.Threading;
+using LogicLayer;
+
+
+
+namespace PresentationLayer
+{
+    class ParamedicinUI_VisBatteristatus
+    {
+        static SerLCD Display;
+        static TWIST Encoder;
+        private int Bstatus;
+        private ShowbatterystatusControl BstatusControl;
+        public ParamedicinUI_VisBatteristatus()
+        {
+            Display = new SerLCD();
+            Encoder = new TWIST();
+            BstatusControl = new ShowbatterystatusControl();
+        }
+        public void visBatteristatus()
+        {
+            Bstatus = BstatusControl.requestbatterystatus();
+            if (Bstatus > 20)
+                displayNormalBatterystatus();
+            else
+                displayLowBatterystatus();
+        }
+        
+        public void displayNormalBatterystatus()
+        {
+            Display.lcdClear();
+            Display.lcdNoBlink();
+            Display.lcdPrint("Batteristatus:");
+            Display.lcdGotoXY(0, 1);
+            string a = Convert.ToString(Bstatus);
+            Display.lcdPrint(a+"%"); // her skal batteristatus tages med
+            Display.lcdGotoXY(0, 2);
+            Display.lcdPrint("Tilbage");
+            Display.lcdCursor();
+            if (Encoder.isPressed())
+                Program.mainMenu();
+        }
+        public void displayLowBatterystatus()
+        {
+            Display.lcdClear();
+            Display.lcdNoBlink();
+            Display.lcdPrint("Batteristatus:");
+            Display.lcdGotoXY(0, 1);
+            string a = Convert.ToString(Bstatus);
+            Display.lcdPrint(a+"%"); //her skal batteristatus tages med
+            Display.lcdGotoXY(0, 2);
+            Display.lcdPrint("Lavt batteriniveau!");
+            Display.lcdGotoXY(0, 3);
+            Display.lcdPrint("Tilbage");
+            Display.lcdCursor();
+            if (Encoder.isPressed())
+                Program.mainMenu();
+        }
+    }
+}
