330a1715ab0a4532c97f7194c606501fd9951bfb
Emil Hertz Degn <au633178@uni.au.dk>
2021-05-26 13:58:33 +0200

    Tilføjet handwarespecifik batteristatus kode.

--- Logic_Layer/ShowBatterystatusControl.cs
+++ Logic_Layer/ShowBatterystatusControl.cs
@@ -7,20 +7,52 @@ using Microsoft.Data.SqlClient;
 
 namespace LogicLayer
 {
-   public class ShowbatterystatusControl
-   {
-      private int batteristatus;
-      private Battery batteryData;
+    public class ShowbatterystatusControl
+    {
+        private int batteristatus;
+        private Battery batteryData;
 
-      public ShowbatterystatusControl()
-      {
-         batteryData = new Battery();
-      }
-      public int requestbatterystatus()
-      {
-         batteristatus = batteryData.getbatterystatus();
-         return batteristatus;
+        public ShowbatterystatusControl()
+        {
+            batteryData = new Battery();
+        }
+        public int requestbatterystatus(int ADCValue)
+        {
+            double sample = ((Convert.ToDouble(ADCValue) / 2048.0) * 6.144);
 
-      }
-   }
+            if (sample >= 2.9)
+            {
+                batteristatus = 100;
+            }
+            else if (sample >= 2.765 && sample < 2.9)
+            {
+                batteristatus = 80;
+            }
+            else if (sample >= 2.701 && sample < 2.765)
+            {
+                batteristatus = 60;
+            }
+            else if (sample >= 2.657 && sample < 2.701)
+            {
+                batteristatus = 40;
+            }
+            else if (sample >= 2.593 && sample < 2.657)
+            {
+                batteristatus = 20;
+            }
+            else if (sample >= 2.506 && sample < 2.593)
+            {
+                batteristatus = 10;
+            }
+            else if (sample < 2.506)
+            {
+                batteristatus = 1;
+            }
+
+            //batteristatus = batteryData.getbatterystatus();
+
+            return batteristatus;
+
+        }
+    }
 }
